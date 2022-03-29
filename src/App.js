6df7ac45ea89328da8b1f880edebc1f1812c49a1
remusiesan <remus.iesan@assist.ro>
2022-03-29 11:22:53 +0300

    Render an object in JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const actor = {
+    name: 'Leonardo Dicaprio',
+    score: 10,
+    hobbies: 'Music and dancing naked in the rain',
+    description: 'He is a good guy with a thick mustahe.'
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {Object.entries(actor).map(([key, val]) => 
+          <li key={key}>{val}</li>
+        )}
+      </ul>
     </div>
   );
 }
