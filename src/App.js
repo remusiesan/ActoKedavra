5d92b91373dc2930fefc0a68e16a6ced5811b7d3
remusiesan <remus.iesan@assist.ro>
2022-03-29 11:36:42 +0300

    My awesome component - hardcoded actor object

--- src/App.js
+++ src/App.js
@@ -3,16 +3,10 @@ import React from "react";
 import Actor from "./components/Actor";
 
 function App() {
-  const actor = {
-    name: 'Leonardo Dicaprio',
-    score: 10,
-    hobbies: 'Music and dancing naked in the rain',
-    description: 'He is a good guy with a thick mustahe.'
-  }
 
   return (
     <div className="App">
-      <Actor actor={actor} />
+      <Actor />
     </div>
   );
 }
--- src/components/Actor.js
+++ src/components/Actor.js
@@ -1,9 +1,16 @@
 import React from "react";
 
 const Actor = (props) => {
+    const actor = {
+        name: 'Leonardo Dicaprio',
+        score: 10,
+        hobbies: 'Music and dancing naked in the rain',
+        description: 'He is a good guy with a thick mustahe.'
+    }
+
     return (
         <ul>
-            {Object.entries(props.actor).map(([key, val]) => 
+            {Object.entries(actor).map(([key, val]) => 
                 <li key={key}>{val}</li>
             )}
         </ul>
