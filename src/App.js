ad467c35c2a962d100cf728cd0692269913a1bd4
remusiesan <remus.iesan@assist.ro>
2022-03-29 11:39:23 +0300

    Pass things from one place to another using props

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import React from "react";
 import Actor from "./components/Actor";
 
 function App() {
+  const actor = {
+    name: 'Leonardo Dicaprio',
+    score: 10,
+    hobbies: 'Music and dancing naked in the rain',
+    description: 'He is a good guy with a thick mustahe.'
+  }
 
   return (
     <div className="App">
-      <Actor />
+      <Actor actor={actor} />
     </div>
   );
 }
--- src/components/Actor.js
+++ src/components/Actor.js
@@ -1,16 +1,10 @@
 import React from "react";
 
 const Actor = (props) => {
-    const actor = {
-        name: 'Leonardo Dicaprio',
-        score: 10,
-        hobbies: 'Music and dancing naked in the rain',
-        description: 'He is a good guy with a thick mustahe.'
-    }
 
     return (
         <ul>
-            {Object.entries(actor).map(([key, val]) => 
+            {Object.entries(props.actor).map(([key, val]) => 
                 <li key={key}>{val}</li>
             )}
         </ul>
